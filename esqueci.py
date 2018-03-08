bd30838af37d795fb141221e41ef58f617be0252
Paulo Alves <juninhpaulo@hotmail.com>
2018-03-08 01:45:27 -0300

    Recursive Unwrapper Function, Not so efficient

--- esqueci.py
+++ esqueci.py
@@ -7,31 +7,30 @@ def generate_file(data, dir):
     generated_file = open(dir, "w")
     generated_file.write(data)
 
-def unwrap_format(f):
-    for key in f:
-        if(type(f[key]) is list):
-            for ff in f[key]:
-                unwrap_format(ff["format"])
-                template = open(ff["template"], 'r').read()
-                data = template.replace('{', '(--').replace('}', '--)').replace('[--', '{').replace('--]', '}').format(**ff["format"]).replace('(--', '{').replace('--)', '}')
-                f[key] = data
-    print(f)
+def unwrap_format(factories):
+    for factory in factories:
+        for key in factory["format"]:
+            if(type(factory["format"][key]) is list):
+                unwrap_format(factory["format"][key])
+                template = open(factory["format"][key][0]["template"], 'r').read()
+
+                factory["format"][key] = template.replace('{', '(--').replace('}', '--)').replace('[--', '{').replace('--]', '}').format(**factory["format"][key][0]["format"]).replace('(--', '{').replace('--)', '}')
 
 def compile_file(template, f):
+    template = open(template, 'r').read()
 
-    f = unwrap_format(f)
-
-    #template = open(template, 'r').read()
-
-    #data = template.replace('{', '(--').replace('}', '--)').replace('[--', '{').replace('--]', '}').format(**f).replace('(--', '{').replace('--)', '}')
-    #return data
+    data = template.replace('{', '(--').replace('}', '--)').replace('[--', '{').replace('--]', '}').format(**f).replace('(--', '{').replace('--)', '}')
+    return data
 
 def compile_schema(schema):
     config = json.load(open('{}.json'.format(schema)))
     project = config["project"]
+    
+    unwrap_format(config["factory"])
+
     for schema in config["factory"]:
         instance = compile_file(schema["template"], schema["format"])
-        #generate_file(instance, schema["name"])
+        generate_file(instance, schema["name"])
 
 
 compile_schema('schema')
